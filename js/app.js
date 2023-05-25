e6183d3005b4699d1c073554e30836b17ee912f8
mashoun <mashoun.me@gmail.com>
2023-05-25 15:30:55 +0300

    swiper object

--- js/app.js
+++ js/app.js
@@ -1,156 +1,17 @@
-import utilities from './utilities.js'
+import swiper from './components/swiper/swiper.js'
+app.component('swiper-container',swiper)
+
+
 const app = Vue.createApp({
     data(){
         return{
-            title:'spectrum real estate',
-            spinner:true,
-            imgs:[
-                './img/sama-min.jpg',
-                './img/door-min.jpg',
-                './img/day3a-min.jpg',
-                './img/room-min.jpg',
-                './img/marfa2-min.jpg',
-                './img/room and door-min.jpg',
-            ],
-            properties:[
-                {
-                    location: 'Hamra',
-                    area: '120',
-                    beds: '2',
-                    bathrooms: '1',
-                    parking: '0',
-                    type: 'Rent',
-                    price: '1,500$',
-                    image: 'https://picsum.photos/1080'
+            title:'Spectrum',
+            heroSwiperInit:{
+                pagination: {
+                    el: '.swiper-pagination',
                 },
-                {
-                    location: 'Achrafieh',
-                    area: '220',
-                    beds: '3',
-                    bathrooms: '2',
-                    parking: '1',
-                    type: 'Sale',
-                    price: '300,000$',
-                    image: 'https://picsum.photos/1081'
-                },
-                {
-                    location: 'Gemmayze',
-                    area: '80',
-                    beds: '1',
-                    bathrooms: '1',
-                    parking: '0',
-                    type: 'Rent',
-                    price: '1,000$',
-                    image: 'https://picsum.photos/1082'
-                },
-                {
-                    location: 'Mar Mikhael',
-                    area: '150',
-                    beds: '2',
-                    bathrooms: '1',
-                    parking: '0',
-                    type: 'Sale',
-                    price: '200,000$',
-                    image: 'https://picsum.photos/1083'
-                },
-                {
-                    location: 'Downtown',
-                    area: '300',
-                    beds: '4',
-                    bathrooms: '2',
-                    parking: '2',
-                    type: 'Rent',
-                    price: '3,500$',
-                    image: 'https://picsum.photos/1084'
-                },
-                {
-                    location: 'Verdun',
-                    area: '200',
-                    beds: '3',
-                    bathrooms: '2',
-                    parking: '1',
-                    type: 'Sale',
-                    price: '250,000$',
-                    image: 'https://picsum.photos/1085'
-                },
-                {
-                    location: 'Ras Beirut',
-                    area: '180',
-                    beds: '3',
-                    bathrooms: '2',
-                    parking: '1',
-                    type: 'Rent',
-                    price: '2,000$',
-                    image: 'https://picsum.photos/1086'
-                },
-                {
-                    location: 'Museum',
-                    area: '100',
-                    beds: '1',
-                    bathrooms: '1',
-                    parking: '0',
-                    type: 'Sale',
-                    price: '120,000$',
-                    image: 'https://picsum.photos/1087'
-                },
-                {
-                    location: 'Sodeco',
-                    area: '250',
-                    beds: '4',
-                    bathrooms: '3',
-                    parking: '2',
-                    type: 'Rent',
-                    price: '2,800$',
-                    image: 'https://picsum.photos/1088'
-                },
-                {
-                    location: 'Raouche',
-                    area: '150',
-                    beds: '2',
-                    bathrooms: '1',
-                    parking: '1',
-                    type: 'Sale',
-                    price: '180,000$',
-                    image: 'https://picsum.photos/1089'
-                },
-                {
-                    location: 'Tabaris',
-                    area: '120',
-                    beds: '2',
-                    bathrooms: '1',
-                    parking: '1',
-                    type: 'Rent',
-                    price: '1,500$',
-                    image: 'https://picsum.photos/1090'
-                },
-                {
-                    location: 'Saifi',
-                    area: '180',
-                    beds: '3',
-                    bathrooms: '2',
-                    parking: '1',
-                    type: 'Sale',
-                    price: '220,000$',
-                    image: 'https://picsum.photos/1091'
-                }
-            ]
+            }
         }
-    },
-    
-    mounted(){
-        document.addEventListener('scroll',e => {
-            // console.log(window.scrollY);
-            if(window.scrollY >= 10) {
-                document.querySelector('header').classList.add('bg-dark')
-            }else document.querySelector('header').classList.remove('bg-dark')
-        })
-        
     }
 })
-
-import title from './components/title/title.js'
-app.component('title-section',title)
-import header from './components/header/header.js'
-app.component('header-section',header)
-
-app.mount('#root')
+app.mount('#app')
---
+++ js/components/swiper/swiper.js
@@ -0,0 +1,9 @@
+import utilities from "../../utilities.js"
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+export default {
+    template: await utilities.getPage('./js/components/swiper/swiper.html'),
+    props: ['id','init'],
+    mounted() {
+        new Swiper(`.${this.id}`,this.init)
+    }
+}
